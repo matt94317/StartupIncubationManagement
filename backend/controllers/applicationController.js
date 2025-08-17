9b14067fa2fb4a4fb0cd5b111926daf5cc3269d2
matt94317 <107838370+matt94317@users.noreply.github.com>
2025-08-17 19:01:38 +1000

    change applicationController.js

--- backend/controllers/applicationController.js
+++ backend/controllers/applicationController.js
@@ -1,62 +1,56 @@
-// Get application
-const application = require("../models/Application");
-const getApplications = async (req, res) => {
+// Get task
+const Task = require("../models/Task");
+const getTasks = async (req, res) => {
   try {
-    const applications = await application.find({ userId: req.user.id });
-    res.json(applications);
+    const tasks = await Task.find({ userId: req.user.id });
+    res.json(tasks);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 // Add application
-const addApplication = async (req, res) => {
+const addTask = async (req, res) => {
   const { title, description, deadline } = req.body;
   try {
-    const application = await Application.create({
+    const task = await Task.create({
       userId: req.user.id,
       title,
       description,
       deadline,
     });
-    res.status(201).json(application);
+    res.status(201).json(task);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 // update application
-const updateApplication = async (req, res) => {
+const updateTask = async (req, res) => {
   const { title, description, completed, deadline } = req.body;
   try {
-    const application = await Application.findById(req.params.id);
-    if (!application)
-      return res.status(404).json({ message: "Application not found" });
-    application.title = title || application.title;
-    application.description = description || application.description;
-    application.completed = completed ?? application.completed;
-    application.deadline = deadline || application.deadline;
-    const updatedApplication = await application.save();
-    res.json(updatedApplication);
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    task.title = title || task.title;
+    task.description = description || task.description;
+    task.completed = completed ?? task.completed;
+    task.deadline = deadline || task.deadline;
+    const updatedTask = await task.save();
+    res.json(updatedTask);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 // delete application
-const deleteApplication = async (req, res) => {
+const deleteTask = async (req, res) => {
   try {
-    const application = await Application.findById(req.params.id);
-    if (!application)
-      return res.status(404).json({ message: "Application not found" });
-    await application.remove();
-    res.json({ message: "Application deleted" });
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    await task.remove();
+    res.json({ message: "Task deleted" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
-module.exports = {
-  getApplications,
-  addApplication,
-  updateApplication,
-  deleteApplication,
-};
+module.exports = { getTasks, addTask, updateTask, deleteTask };
